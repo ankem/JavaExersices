7699e7ba260d0b8d9dfa24486ea6914f0f609c1e
vjayanthi <jayanthi.varanasi@gmail.com>
2016-11-02 22:35:45 -0500

    Created Primitives class and Runner class

---
+++ jayanthi/src/main/java/com/yash/exercise/Primitives.java
@@ -0,0 +1,27 @@
+package com.yash.exercise;
+
+/**
+ * Created by jayanthi on 11/2/2016.
+ */
+public class Primitives {
+
+    public void printMinMax() {
+
+        System.out.println("min value of byte  " + Byte.MIN_VALUE);
+        System.out.println("max value of byte " + Byte.MAX_VALUE);
+        System.out.println("min value of int " + Integer.MIN_VALUE);
+        System.out.println("max value of int " + Integer.MAX_VALUE);
+        System.out.println("min value of long " + Long.MIN_VALUE);
+        System.out.println("max value of long " + Long.MAX_VALUE);
+        System.out.println("min value of float " + Float.MIN_VALUE);
+        System.out.println("min value of float " + Float.MAX_VALUE);
+        System.out.println("min value of double " + Double.MIN_VALUE);
+        System.out.println("min value of double " + Double.MAX_VALUE);
+
+
+
+
+
+    }
+
+}
---
+++ jayanthi/src/main/java/com/yash/exercise/Runner.java
@@ -0,0 +1,13 @@
+package com.yash.exercise;
+
+/**
+ * Created by jayanthi on 11/2/2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        Primitives primitives = new Primitives();
+
+        primitives.printMinMax();
+
+    }
+}
