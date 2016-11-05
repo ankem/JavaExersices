64ca54f12ba191f62f44f268a6be1942d6266338
ushaankem <usharao@live.com>
2016-11-05 06:47:00 -0500

    print max and min of all primitives (#8)
    * print max and min of all primitives
    
    * min-max
    
    * deleted unnecessary space min-max

---
+++ usha/src/main/java/com/usha/java/Primitive.java
@@ -0,0 +1,26 @@
+package com.usha.java;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by ankem_000 on 11/3/2016.
+ */
+public class Primitive {
+    public void printMinMax()
+    {
+        System.out.println("min value of byte is =" +Byte.MIN_VALUE);
+        System.out.println("max value of byte is " + Byte.MAX_VALUE);
+        System.out.println("min value of short is " + Short.MIN_VALUE);
+        System.out.println("max value of short is " + Short.MAX_VALUE);
+        System.out.println("min value of int is" + Integer.MIN_VALUE);
+        System.out.println("max value of Int is" + Integer.MAX_VALUE);
+        System.out.println("min value of long is" + Long.MIN_VALUE);
+        System.out.println("max value of long is" + Long.MAX_VALUE );
+        System.out.println("min value of float is" + Float.MIN_VALUE);
+        System.out.println("max value of float is" +Float.MAX_VALUE);
+        System.out.println("min value of double is" + Double.MIN_VALUE);
+        System.out.println("max value of double is" + Double.MAX_VALUE);
+        System.out.println("min value of char is" + Character.MIN_VALUE);
+        System.out.println("max value of char is" + Character.MAX_VALUE);
+    }
+}
---
+++ usha/src/main/java/com/usha/java/Runner.java
@@ -0,0 +1,8 @@
+package com.usha.java;
+
+public class Runner {
+    public static void main(String[] args){
+        Primitive p = new Primitive();
+        p.printMinMax();
+    }
+}
