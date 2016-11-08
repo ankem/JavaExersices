4b540c706a947355fc07a5d1d9dcf807798a1ab1
vjayanthi <jayanthi.varanasi@gmail.com>
2016-11-07 21:56:09 -0600

    Program for single string to show output One, Two, Three

---
+++ jayanthi/src/main/java/com/yash/exercise/SingleString.java
@@ -0,0 +1,17 @@
+package com.yash.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jayanthi on 11/7/2016.
+ */
+public class SingleString {
+
+    public static void main(String[] args) {
+        String s = "One,\n" + "Two,\n" + "Three";
+
+        System.out.println(s);
+
+    }
+}
